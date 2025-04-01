73a3728ae6c3ceddde924509738d5c804a061ad3
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-04-01 07:48:01 -0400

    imported images for scroll shortcut, created timeline bar

---
+++ src/components/timeline-bar.tsx
@@ -0,0 +1,12 @@
+import LeftArrow from '../assets/graphics/arrow-left.png'
+import RightArrow from '../assets/graphics/arrow-right.png'
+
+export default function Timeline(){
+    return(
+        <div className="relative flex flex-row justify-center items-center">
+            <img id="left-arrow" src={LeftArrow} alt="move-left" className='w-[50px] mr-[20px] ml-[4.8vw] cursor-pointer'/>
+            <div className="h-[0.4rem] w-[100%] bg-black"></div>
+            <img id="right-arrow" src={RightArrow} alt="move-right" className='w-[50px] ml-[20px] mr-[4.8vw] cursor-pointer'/>
+        </div>
+    )
+}
