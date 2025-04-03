f0b0522e44df9937118dec810a42906de1c402af
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-04-03 12:45:43 -0400

    Changed visuals of thumbnail style slightly

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from "react";
 import Timeline from "./components/timeline";
 import TimelineBar from "./components/timeline-bar";
-import Thumbnail from "./components/thumbnail";
 
 function App() {
   return (
-    <div className="relative flex items-center w-[100vw] h-[100vh]">
+    <div className="relative flex h-[100vh] w-[100vw] items-center">
       <div className="absolute w-[100%]">
-        <Timeline/>  
-      </div>    
+        <Timeline />
+      </div>
       <TimelineBar />
     </div>
   );
-  // <div className="relative flex items-center w-[100vw] h-[100vh]">
-  //     <Thumbnail 
-  //       text="Lorem Ipsum"
-  //       image="/sample1.jpg"
-  // //     />
-  // //   </div>
-  // );
 }
 
 export default App;
