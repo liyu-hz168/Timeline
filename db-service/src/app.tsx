a2be60b9eef0d45cb622f9b6352d74fac7f97ace
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-05-06 15:59:02 -0400

    set up mongodb database

---
+++ db-service/src/app.tsx
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import memoryRoutes from "./routes/memoryRoutes";
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://mongodb:27017/memorydb")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.use("/api", memoryRoutes);
+
+app.listen(5001, () => {
+  console.log("DB Service running on port 5001");
+});
---
+++ db-service/src/models/MemoryCard.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const MemoryCardSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
+  date: { type: String, required: true },
+  position: {
+    x: { type: Number, required: true },
+    y: { type: Number, required: true },
+  },
+  memoryID: { type: String, required: true },
+});
+
+export default mongoose.model("MemoryCard", MemoryCardSchema);
