b9e5e64dbb4bd0d8140b5858491bbddcdae721ef
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-05-08 22:45:07 -0400

    made changes to font

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,12 +12,12 @@ const NavBar = () => {
     <div className="grid w-[100vw] grid-cols-3 items-center justify-center p-4 text-center text-black">
       {/* App name */}
       <div>
-        <h1 className="font-editorial scale-y-[1.1] text-left text-4xl tracking-[-2px]">
+        <h1 className="scale-y-[1.1] text-left font-editorial text-4xl tracking-[-2px]">
           <span>Timeline.</span>
         </h1>
       </div>
       {/* View Navigation */}
-      <div className="font-editorial flex items-center gap-4 justify-self-center text-4xl">
+      <div className="flex items-center gap-4 justify-self-center font-editorial text-4xl">
         <button className="scale-y-[1.1] hover:underline" id="week-button">
           week
         </button>
@@ -33,7 +33,7 @@ const NavBar = () => {
       {/* Buttons TODO: add zooming frame */}
       <div className="text-right">
         <button
-          className="font-editorial mr-4 rounded-full bg-gray-700 px-3 py-1 text-xl text-white hover:bg-gray-500"
+          className="mr-4 rounded-full bg-gray-700 px-3 py-1 font-editorial text-xl text-white hover:bg-gray-500"
           onClick={() => setIsSettingsOpen(true)}
         >
           settings
@@ -43,7 +43,7 @@ const NavBar = () => {
           onClose={() => setIsSettingsOpen(false)}
         />
         <button
-          className="font-editorial rounded-full bg-gray-700 px-3 py-1 text-xl text-white hover:bg-gray-500"
+          className="rounded-full bg-gray-700 px-3 py-1 font-editorial text-xl text-white hover:bg-gray-500"
           onClick={() => navigate(`/`)}
         >
           sign out
