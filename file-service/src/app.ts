8cc90cad7b6444ca5d8e2848d0fc4b311c9d5cc6
liyu-hz168 <liyu2004huang@gmail.com>
2025-05-09 00:33:04 -0400

    quick fix

--- file-service/src/app.ts
+++ file-service/src/app.ts
@@ -4,6 +4,10 @@ import multer, { FileFilterCallback } from "multer";
 import path from "path";
 import fs from "fs";
 import { promisify } from "util";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
